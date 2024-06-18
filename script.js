4d1b2ca1cf780ae177268d2b24801037e1d9011c
Mirolla <144719700+mirollaf@users.noreply.github.com>
2024-06-18 02:14:16 +0000

    ratings and contacts!

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const products = [
         {
@@ -7,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             price: 29.99,
             image: 'images/car1.jpg',
             description: 'This heirloom-quality wooden car is a timeless treasure. Handcrafted from domestic and exotic hardwoods with a clear lacquer finish, this unique car will inspire generations of imaginative play. Please note potential choking hazards for small children.',
+            rating: '⭐⭐⭐⭐⭐',
 
 
         },
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
             price: 39.99,
             image: 'images/plane1.jpg',
             description: 'Soar through imaginative skies with this classic wooden airplane. Handcrafted from sustainable Baltic birch wood with a safe, natural harvest finish and a spinning propeller. Measures 3.5"H x 7"L x 7"W.',
+            rating: '⭐⭐⭐⭐⭐',
 
         },
         {
@@ -24,14 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
             price: 49.99,
             image: 'images/train2.jpg',
             description: 'Embark on a charming journey with this beautiful handcrafted wooden train set. Engine and three interchangeable cars boast intricate details made from real beech wood. Large size (84cm L x 11cm H x 13cm W) with moving wheels and a fully ecological design.',
-
+            rating: '⭐⭐⭐⭐⭐',
         },
         {
             id: 4,
             name: 'Wooden Boat',
             price: 39.99,
             image: 'images/boat2.jpg',
-            description: 'Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety. Includes two peg "lobster people." Size: 10.5"W x 3.5"H.'
+            description: 'Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety. Includes two peg "lobster people." Size: 10.5"W x 3.5"H.',
+            rating: '⭐⭐⭐⭐⭐',
 
         },
         {
@@ -39,7 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Wooden Block Set ',
             price: 29.99,
             image: 'images/block2.jpg',
-            description: 'Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate (13” L x 12” W x 2” H).'
+            description: 'Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate (13” L x 12” W x 2” H).',
+            rating: '⭐⭐⭐⭐⭐',
 
         },
         {
@@ -47,11 +50,12 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Product 6',
             price: 19.99,
             image: 'https://via.placeholder.com/150',
-            description: 'Lorem Ipsum'
+            description: 'Lorem Ipsum',
+            rating: '⭐⭐⭐⭐⭐',
 
         },
     ];
- const productCarousel = document.getElementById('product-carousel');
+    const productCarousel = document.getElementById('product-carousel');
 
     products.forEach(product => {
         const productCard = document.createElement('div');
@@ -62,6 +66,8 @@ document.addEventListener('DOMContentLoaded', () => {
             <h2>${product.name}</h2>
             <p>$${product.price.toFixed(2)}</p>
             <p>${product.description}</p>
+            <p>${product.rating}</p>
+
             <button onclick="addToCart(${product.id})">Add to Cart</button>
         `;
 
